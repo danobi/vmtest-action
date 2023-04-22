ecc2c8265284b4f63b318d31557cb91d572e7bf9
Daniel Xu <dxu@dxuuu.xyz>
2023-04-21 18:22:05 -0600

    Parse booleans more intelligently

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+use anyhow::{anyhow, Result};
 use clap::Parser;
 
 #[derive(Parser, Debug)]
 struct Args {
     name: String,
     image: String,
+    // The clap directives are to parse "true"/"false" as bools while
+    // also working around the fact that by default clap treats bools
+    // as flags and not positional parameters.
+    #[clap(action = clap::ArgAction::Set, value_parser = parse_bool)]
     uefi: bool,
     kernel: String,
     kernel_args: String,
     command: String,
+}
+
+fn parse_bool(s: &str) -> Result<bool> {
+    match s.to_lowercase().as_ref() {
+        "true" => Ok(true),
+        "false" => Ok(false),
+        _ => Err(anyhow!("Invalid boolean value")),
+    }
 }
 
 fn main() {
