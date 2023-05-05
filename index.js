bf62eb2ac7d96a2296a3406099ce23520602636f
Daniel Xu <dxu@dxuuu.xyz>
2023-05-05 15:47:08 -0600

    Stub out structure

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const vmtest = require('./vmtest');
 
 try {
-    const name = core.getInput('name');
-    const image = core.getInput('image');
-    const uefi = core.getInput('uefi');
-    const kernel = core.getInput('kernel');
-    const kernel_args = core.getInput('kernel_args');
-    const command = core.getInput('command');
-
-    console.log(`name=${name}, image=${image}, uefi=${uefi}, kernel=${kernel}, kernel_args=${kernel_args}, command=${command}`);
-
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    vmtest.main();
 } catch (error) {
     core.setFailed(error.message);
 }
---
+++ vmtest.js
@@ -0,0 +1,46 @@
+const core = require('@actions/core');
+const fs = require('fs/promises');
+const github = require('@actions/github');
+
+// Check if the current runner is ubuntu. If not, throws an exception
+async function checkOnUbuntu(osRelease) {
+    const data = await fs.readFile(osRelease, {
+        encoding: 'utf8'
+    });
+    const lines = data.toString().split('\n');
+
+    for (var i = 0; i < lines.length; i++) {
+        var parts = lines[i].split('=');
+        if (parts[0] == 'ID' && parts[1] == 'ubuntu') {
+            return;
+        }
+    }
+
+    throw new Error('This action only works on Ubuntu runners');
+}
+
+// Install required vmtest dependencies
+async function installDependencies() {}
+
+async function materializeConfig(args, configFile) {}
+
+async function runVmtest(configFile) {}
+
+async function main() {
+    var args = {
+        name: core.getInput('name'),
+        image: core.getInput('image'),
+        uefi: core.getInput('uefi').toLowerCase() === 'true',
+        kernel: core.getInput('kernel'),
+        kernel_args: core.getInput('kernel_args'),
+        command: core.getInput('command'),
+    };
+
+    core.debug(`args=${args}`);
+    await checkOnUbuntu('/etc/os-release');
+    await installDependencies();
+    await materializeConfig(args, './vmtest.toml');
+    await runVmtest('./vmtest.toml');
+}
+
+module.exports = { checkOnUbuntu, materializeConfig };
