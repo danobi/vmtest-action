d51b322edff234f0a59572018d00c7744f2c4ec4
Daniel Xu <dxu@dxuuu.xyz>
2023-05-05 16:49:39 -0600

    Add config materialization tests

--- index.test.js
+++ index.test.js
@@ -1,6 +1,8 @@
+const fs = require('fs');
+const tmp = require('tmp');
 const vmtest = require('./vmtest');
 
-test('validate valid args', async () => {
+test('validate image args', async () => {
     const args = {
         name: 'test',
         image: './foo.img',
@@ -10,6 +12,34 @@ test('validate valid args', async () => {
         command: '/bin/true',
     };
 
-    await vmtest.materializeConfig(args, './vmtest.toml');
-    // XXX: add actual test logic
+    const config = tmp.fileSync();
+    await vmtest.materializeConfig(args, config.name);
+    const contents = fs.readFileSync(config.name, 'utf8');
+    const expected = '[[target]]\n' +
+        'name = "test"\n' +
+        'image = "./foo.img"\n' +
+        'uefi = true\n' +
+        'command = "/bin/true"';
+    expect(contents).toBe(expected);
+});
+
+test('validate kernel args', async () => {
+    const args = {
+        name: 'test',
+        image: '',
+        uefi: 'false',
+        kernel: './bzImage-6.0',
+        kernel_args: 'console=ttyS0',
+        command: '/bin/true',
+    };
+
+    const config = tmp.fileSync();
+    await vmtest.materializeConfig(args, config.name);
+    const contents = fs.readFileSync(config.name, 'utf8');
+    const expected = '[[target]]\n' +
+        'name = "test"\n' +
+        'kernel = "./bzImage-6.0"\n' +
+        'kernel_args = "console=ttyS0"\n' +
+        'command = "/bin/true"';
+    expect(contents).toBe(expected);
 });
--- vmtest.js
+++ vmtest.js
@@ -21,7 +21,27 @@ async function checkOnUbuntu(osRelease) {
 // Install required vmtest dependencies
 async function installDependencies() {}
 
-async function materializeConfig(args, configFile) {}
+async function materializeConfig(args, configFile) {
+    var lines = [];
+    lines.push('[[target]]');
+    lines.push(`name = "${args.name}"`);
+    if (args.image.length) {
+        lines.push(`image = "${args.image}"`);
+    }
+    if (args.uefi.toLowerCase() == 'true') {
+        lines.push('uefi = true');
+    }
+    if (args.kernel.length) {
+        lines.push(`kernel = "${args.kernel}"`);
+    }
+    if (args.kernel_args.length) {
+        lines.push(`kernel_args = "${args.kernel_args}"`);
+    }
+    lines.push(`command = "${args.command}"`);
+
+    var contents = lines.join('\n');
+    await fs.writeFile(configFile, contents);
+}
 
 async function runVmtest(configFile) {}
 
@@ -29,7 +49,7 @@ async function main() {
     var args = {
         name: core.getInput('name'),
         image: core.getInput('image'),
-        uefi: core.getInput('uefi').toLowerCase() === 'true',
+        uefi: core.getInput('uefi'),
         kernel: core.getInput('kernel'),
         kernel_args: core.getInput('kernel_args'),
         command: core.getInput('command'),
